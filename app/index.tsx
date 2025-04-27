94837b4a63be5ac9d5cdc81a05f63f06f1ecb5f8
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2025-04-27 15:50:04 -0300

    feat: implement marker creation and location fetching in Index component

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import IndexBottomSheet from "@/components/BottomSheet";
-import CustomButton from "@/components/Button";
+import { CustomFloatingButton } from "@/components/Button";
 import Map from "@/components/Map";
 import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
@@ -7,6 +7,23 @@ import MapView, { LatLng } from "react-native-maps";
 
 export default function Index() {
   const [userMarker, setUserMarker] = useState<LatLng>();
+  const [location, setLocation] = useState();
+
+  async function createMarker(latLng: LatLng) {
+    console.log("hey");
+    const res = await fetch(
+      `https://nominatim.openstreetmap.org/reverse?lat=${latLng.latitude}&lon=${latLng.longitude}&format=json`,
+      {
+        headers: {
+          "User-Agent": "markaaiapp", // optional but recommended for Nominatim
+        },
+      }
+    );
+    const data = await res.json();
+    console.log(data);
+    setLocation(data);
+    setUserMarker(latLng);
+  }
 
   return (
     <View
@@ -16,9 +33,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      {userMarker && <CustomButton />}
-      <Map setUserMarker={setUserMarker} userMarker={userMarker} />
-      <IndexBottomSheet />
+      {/* {userMarker && <CustomFloatingButton />} */}
+      <Map createMarker={createMarker} userMarker={userMarker} />
+      <IndexBottomSheet location={location} />
     </View>
   );
 }
